2563cfcd588d90b88d2ef22f4722339911de6984
Meet Gojiya <meetgojiya@Meets-MacBook-Pro.local>
2025-05-31 12:59:24 +0530

    some fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,24 @@
 import "./globals.css";
-import { ThemeProvider } from "../components/ThemeContext";
+import { Providers } from "./providers";
 import Header from "../components/Header";
-import Footer from "../components/Footer"; // Add this
+import Footer from "../components/Footer";
 
 export const metadata = {
   title: "Stock Market Copilot",
   description: "RAG based Stock Market Copilot",
+  icons: {
+    icon: "/stockmarket.jpg",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-      <link rel="icon" href="/stockmarket.jpg" />
-      </head>
-      <body className="bg-gradient-to-b from-slate-50 to-zinc-100 dark:from-zinc-900 dark:to-zinc-950 min-h-screen flex flex-col">
-      <ThemeProvider>
-          <Header />
-          <main className="flex-1">{children}</main>
-          <Footer /> {/* Place it here for global bottom alignment */}
-          <ThemeProvider>
-      </body>
-    </html>
+    <>
+      <Providers>
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </Providers>
+    </>
   );
 }
